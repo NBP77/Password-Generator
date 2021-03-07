afead9b31b93fef3d412d2daae1e9b4e82d976c5
NBP77 <nbp77@outlook.com>
2021-03-07 17:57:22 -0500

    Added the prompt section

--- Assets/script.js
+++ Assets/script.js
@@ -1,29 +1,30 @@
 // Assignment Code
 
-const resultId = document.getElementById('password');
-const lengthId = document.getElementById('password');
-const lowerCaseId = document.getElementById('password');
-const upperCaseId = document.getElementById('password');
-const numbersId = document.getElementById('password');
-const specialCharId = document.getElementById('password');
-const generateId = document.getElementById('password');
+var passwordLength = prompt("How many characters would you like your password to be?")
 
-
-
-
-const ranFunc = {
-  lowerCase: getRanLowerCase,
-  upperCase: getRanUpperCase,
-  number: getRanNumber,
-  specialChar: getRanSpecChar,
-
+if (passwordLength < 8 || passwordLength > 128){
+  alert("Length must be 8-128")
 }
 
-generateId.addEventListener('click', () => {
-  const length = lengthId.value;
+if (passwordLength >= 8 && passwordLength <= 128){
+  var confirmUpper = confirm("Would you like to use uppercase letters?")
+  var confirmLower = confirm("Would you like to use lowercase letters?")
+  var confirmNumber = confirm("Would you like to use numbers?")
+  var confirmCharacters = confirm("Would you like to use special characters?")
 
-  console.log(length);
-});
+
+if(confirmUpper != true && confirmLower != true && confirmNumber != true && confirmCharacters != true){
+  alert("you must select at least one character type!")
+}
+
+
+
+
+// generateId.addEventListener('click', () => {
+//   const length = lengthId.value;
+
+//   console.log(length);
+// });
 
 // Generator Functions
 function getRanLowerCase () {
@@ -54,29 +55,38 @@ console.log(getRanSpecChar());
 
 
 
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-
-//  var passwordLength = prompt("How long is you password");
-//  if (passwordLength == null || passwordLength == "") {
-//  txt = "Password not long enough.";
-//  } else {
-//    txt = "Do you want lowercase letters?";
 // }
 
-function promptMe(){
-var passwordLength = prompt("How long do you want your password")
-};
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+
+
+
+// // const resultId = document.getElementById('password');
+// // const lengthId = document.getElementById('password');
+// // const lowerCaseId = document.getElementById('password');
+// // const upperCaseId = document.getElementById('password');
+// // const numbersId = document.getElementById('password');
+// // const specialCharId = document.getElementById('password');
+// // const generateId = document.getElementById('password');
+
+
+
+
+// const ranFunc = {
+//   lowerCase: getRanLowerCase,
+//   upperCase: getRanUpperCase,
+//   number: getRanNumber,
+//   specialChar: getRanSpecChar,
+
+}
