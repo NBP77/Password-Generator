14dfaa39d69b7278da9cfa4ab98af1231ee95ad5
NBP77 <nbp77@outlook.com>
2021-03-12 09:48:04 -0500

    The password is now generated on the screen and the application works, the only thing I need to improve is the prompt not showing up as soon as you open or refresh the page

--- Assets/script.js
+++ Assets/script.js
@@ -2,177 +2,171 @@
 
 // Links HTML element
 
-var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-console.log(upperCaseArray)
+var upperCaseArray = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+console.log(upperCaseArray);
 
-var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-console.log(lowerCaseArray)
+var lowerCaseArray = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+console.log(lowerCaseArray);
 
-var numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,]
-console.log(numberArray)
+var numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(numberArray);
 
-var specialCharacterArray = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{"]
-console.log(specialCharacterArray)
+var specialCharacterArray = [
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "(",
+  ")",
+  "*",
+  "+",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "]",
+  "^",
+  "_",
+  "{",
+];
+console.log(specialCharacterArray);
 
-
-var potentialArray = []
-var finalPasswordArray = []
-console.log(finalPasswordArray)
+var potentialArray = [];
+var finalPasswordArray = [];
+console.log(finalPasswordArray);
 // potentialArray.push(?)
 
-// function for selecting random characters from arrays 
+// function for selecting random characters from arrays
 // function randomGenerator(aryIndex){
 //   var randomIndex = Math.floor(Math.random() * aryIndex.length);
 //   return randomIndex;
 // }
 
+document.getElementById("password").value = doPassword();
+
 function doPassword() {
-document.getElementById("password").value = password;
-
-// Setting up the prompt to start the password process
-var passwordLength = prompt("How many characters would you like your password to be?")
-
-// This says if the password length is less than 8 or greater than 128 an alret pops up that
-// your password length needs to be in between those numbers
-if (passwordLength < 8 || passwordLength > 128){
-  alert("Length must be 8-128")
-}
-
-// This goes to the next step and ask the follwing questions to confirm
-if (passwordLength >= 8 && passwordLength <= 128){
-  var confirmUpper = confirm("Would you like to use uppercase letters?")  
-  var confirmLower = confirm("Would you like to use lowercase letters?")
-  var confirmNumber = confirm("Would you like to use numbers?")
-  var confirmSpecialCharcters = confirm("Would you like to use a special character?")
-// requires randomization
-}
-
-// // If none of those choices are selected then an alert pops up
-
-// if(confirmUpper != true && confirmLower != true && confirmNumber != true && confirmSpecialCharcters != true){
-//   alert("you must select at least one character type!")
-// };
-
-if (confirmUpper) {
-  potentialArray = potentialArray.concat(upperCaseArray);
-}
-if (confirmLower) {
-  potentialArray = potentialArray.concat(lowerCaseArray);
-}
-if (confirmNumber) {
-  potentialArray = potentialArray.concat(numberArray);
-}
-if (confirmSpecialCharcters) {
-  potentialArray = potentialArray.concat(specialCharacterArray);
-}
-console.log(passwordLength);
-var randomCharacter;
-for (var i = 0; i < passwordLength; i++) {
-
-randomCharacter = potentialArray[Math.floor(Math.random() * potentialArray.length)]
-console.log(randomCharacter);
-
-finalPasswordArray.push(randomCharacter); 
-
-}
-
-return finalPasswordArray.join("");
-
-}
-
-
-console.log("!!!!")
-
-
-// turn final password array into a string and console log it 
-
-
-// DOM Elements 
-
-// const resultEl = document.getElementById('password'); 
-
-// document.getElementById('generate').addEventListener('click', () => {
-//   const hasLower = confirmLower.true;
-//   const hasUpper = confirmUpper.true;
-//   const hasNumber = confirmNumber.true;
-//   const hasChar = confirmCharacters.true;
-//   resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasChar, passwordLength);
-// });
-
-// // const ranFunc = {
-// //   lowerCase: getRanLowerCase,
-// //   upperCase: getRanUpperCase,
-// //   number: getRanNumber,
-// //   specialChar: getRanSpecChar
-
-// // }
-
-// function generatePassword(lowerCase, upperCase, number, specialChar, length) {
-//   let generatedPassword = [];
-//   const typesCount = lowerCase + upperCase + number + specialChar;
-//   const typesArr = [{lowerCase}, {upperCase}, {number}, {specialChar}].filter(items => Object.values(items)[0]);
-
   
-//   console.log(generatedPassword)
+  // Setting up the prompt to start the password process
+  var passwordLength = prompt(
+    "How many characters would you like your password to be?"
+  );
 
-// // Loop
+  // This says if the password length is less than 8 or greater than 128 an alret pops up that
+  // your password length needs to be in between those numbers
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Length must be 8-128");
+  }
 
-//     for(let i=0; i<length; i+=typesCount) {
-//           typesArr.forEach(type => {
-//             const funcName = Object.keys(type)[0];
-//             generatedPassword += ranFunc[funcName]();
-//           });
-//     }
+  // This goes to the next step and ask the follwing questions to confirm
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    var confirmUpper = confirm("Would you like to use uppercase letters?");
+    var confirmLower = confirm("Would you like to use lowercase letters?");
+    var confirmNumber = confirm("Would you like to use numbers?");
+    var confirmSpecialCharcters = confirm(
+      "Would you like to use a special character?"
+    );
+    // requires randomization
+  }
 
-//       const finalPassword = generatedPassword.slice(0, length);
+  // // If none of those choices are selected then an alert pops up
 
-//       console.log(finalPassword)
-      
-//       return finalPassword;
+  if(confirmUpper != true && confirmLower != true && confirmNumber != true && confirmSpecialCharcters != true){
+    alert("you must select at least one character type!")
+  };
 
-      
+  if (confirmUpper) {
+    potentialArray = potentialArray.concat(upperCaseArray);
+  }
+  if (confirmLower) {
+    potentialArray = potentialArray.concat(lowerCaseArray);
+  }
+  if (confirmNumber) {
+    potentialArray = potentialArray.concat(numberArray);
+  }
+  if (confirmSpecialCharcters) {
+    potentialArray = potentialArray.concat(specialCharacterArray);
+  }
+  console.log(passwordLength);
+  var randomCharacter;
+  for (var i = 0; i < passwordLength; i++) {
+    randomCharacter =
+      potentialArray[Math.floor(Math.random() * potentialArray.length)];
+    console.log(randomCharacter);
 
-     
+    finalPasswordArray.push(randomCharacter);
+  }
 
+  console.log(finalPasswordArray);
 
+  return finalPasswordArray.join("");
+}
 
-
-// generateId.addEventListener('click', () => {
-//   const length = lengthId.value;
-
-//   console.log(length);
-// });
-
-// Generator Functions/ Random functions to get lowercase, uppercase, number and special character//
-
-// function getRanLowerCase () {
-//   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-// }
-
-
-// console.log(getRanLowerCase()); 
-
-// function getRanUpperCase () {
-//   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-// }
-
-// console.log(getRanUpperCase());
-
-// function getRanNumber () {
-//   return Math.floor(Math.random() * 100);
-// }
-
-// console.log(getRanNumber()); 
-
-// function getRanSpecChar (){
-//   const special = '!"#$%&()*+,-./:;<=>?@[]\^_{|}~' ;
-//   return special[Math.floor(Math.random() * special.length)]
-// }
-
-// console.log(getRanSpecChar());
-
+console.log("!!!!");
 
 
+// // Assignment Code
 // var generateBtn = document.querySelector("#generate");
 
 // // Write password to the #password input
@@ -186,15 +180,3 @@ console.log("!!!!")
 
 // // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
-
-
-
-
-
-
